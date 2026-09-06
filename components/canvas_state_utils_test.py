59b51de8f7f14d00ae8e36c7ede49e67969dc18e
silent107 <86247687+silent107@users.noreply.github.com>
2026-09-06 11:05:17 -0400

    Remove canvas_utils file, migrate to respective

--- components/canvas_state_utils_test.py
+++
@@ -1,5 +0,0 @@
-from components.canvas_state_utils import clamp_surface_placement
-
-
-def test_clamp_surface_placement_bounds_coordinates() -> None:
-    assert clamp_surface_placement(-1, 120) == {"left_pct": 2.0, "top_pct": 98.0}
